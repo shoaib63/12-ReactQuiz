662c18358e9fbc3c1d1d017b1a3f53cf45a2d150
shoaib63 <mirakishoaib@gmail.com>
2024-08-10 12:45:18 +0330

    end of 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-function App() {}
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+
+function App() {
+
+    return(
+        <>
+        <Header />
+        <main>
+            <Quiz />
+
+        </main>
+        
+        </>
+    );
+}
 
 export default App;
